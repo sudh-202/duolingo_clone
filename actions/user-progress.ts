3a21db1ec1e68a1c8a825529b4caeab142390408
sudh-202 <srivastavasudhanshu202@gmail.com>
2024-05-17 17:07:43 +0530

    Challenge Fishish Screen

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import db from "@/db/drizzle";
-import { getCoureseById, getUserProgress } from "@/db/queries";
-import { userProgress } from "@/db/schema";
+import { getCourseById, getUserProgress } from "@/db/queries";
+import { challengeProgress, challenges, userProgress } from "@/db/schema";
 import { auth, currentUser } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
@@ -15,7 +16,7 @@ export const upsertUserProgress = async (courseId: number) => {
     throw new Error("Unauthorized");
   }
 
-  const course = await getCoureseById(courseId);
+  const course = await getCourseById(courseId);
 
   if (!course) {
     throw new Error("Course not found");
@@ -49,4 +50,49 @@ export const upsertUserProgress = async (courseId: number) => {
   revalidatePath("/learn");
   revalidatePath("/courses");
   redirect("/learn");
+};
+
+export const reduceHearts = async (challengeId: number) => {
+  const { userId } = auth();
+
+  if (!userId) throw new Error("Unauthorized.");
+
+  const currentUserProgress = await getUserProgress();
+  // const userSubscription = await userSubscription();
+
+  const challenge = await db.query.challenges.findFirst({
+    where: eq(challenges.id, challengeId),
+  });
+  
+  if (!challenge) throw new Error("Challenge not found.");
+
+  const lessonId = challenge.lessonId;
+
+  const existingChallengeProgress = await db.query.challengeProgress.findFirst({
+    where: and(
+      eq(challengeProgress.userId, userId),
+      eq(challengeProgress.challengeId, challengeId)
+    ),
+  });
+
+  const isPractice = !!existingChallengeProgress;
+  if (isPractice) return { error: "practice" };
+
+  if (!currentUserProgress) throw new Error("User progress not found.");
+
+  // if (userSubscription?.isActive) return { error: "subscription" };
+
+  if (currentUserProgress.hearts === 0) return { error: "hearts" };
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: Math.max(currentUserProgress.hearts - 1, 0),
+    })
+    .where(eq(userProgress.userId, userId));
+        revalidatePath("/shop");
+        revalidatePath("/learn");
+        revalidatePath("/quests");
+        revalidatePath("/leaderboard");
+        revalidatePath(`/lesson/${lessonId}`);
 };
--- scripts/seeds.ts
+++ scripts/seeds.ts
@@ -99,6 +99,20 @@ const main = async () => {
         order: 1,
         question: 'Which one of these is the "the man"?',
       },
+      {
+        id: 2,
+        lessonId: 1, // Nouns
+        type: "ASSIST",
+        order: 2,
+        question: '"the man"',
+      },
+      {
+        id: 3,
+        lessonId: 1, // Nouns
+        type: "SELECT",
+        order: 3,
+        question: 'Which one of these is the "the robot"?',
+      },
     ]);
 
     await db.insert(schema.challengeOptions).values([
@@ -122,6 +136,75 @@ const main = async () => {
         correct: false,
         text: "el robot",
         audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 2, // "the man"?
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        challengeId: 2,
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        challengeId: 2,
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 3, // Which one of these is the "the robot"?
+        imageSrc: "/man.svg",
+        correct: false,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3",
+      },
+      {
+        challengeId: 3,
+        imageSrc: "/woman.svg",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3",
+      },
+      {
+        challengeId: 3,
+        imageSrc: "/robot.svg",
+        correct: true,
+        text: "el robot",
+        audioSrc: "/es_robot.mp3",
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 4,
+        lessonId: 2, // Verbs
+        type: "SELECT",
+        order: 1,
+        question: 'Which one of these is the "the man"?',
+      },
+      {
+        id: 5,
+        lessonId: 2, // Verbs
+        type: "ASSIST",
+        order: 2,
+        question: '"the man"',
+      },
+      {
+        id: 6,
+        lessonId: 2, // Verbs
+        type: "SELECT",
+        order: 3,
+        question: 'Which one of these is the "the robot"?',
       },
     ]);
 
